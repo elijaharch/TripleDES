4d7146ea4ab1746052bfe6aa13e6d2246ba1ac86
elijaharch <70805030+elijaharch@users.noreply.github.com>
2022-12-28 00:41:38 +0300

    1st func works fine

--- Python_DES3/tripledes2.py
+++ Python_DES3/tripledes2.py
@@ -1,35 +1,49 @@
 from Crypto.Cipher import DES3
-from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
 import base64
+import random
+import string
 
 BS = DES3.block_size
 pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
-unpad = lambda s : s[0:-ord(s[-1])]
+unpad = lambda s: s[0:-s[-1]]
+
 
 def encrypt(plaintext, key):
     plaintext = pad(plaintext)
-    cipher = DES3.new(key, DES3.MODE_ECB)
+    cipher = DES3.new(key.encode(), DES3.MODE_ECB)
     ciphertext = cipher.encrypt(plaintext.encode('utf-8'))
-    return ciphertext
+    ciphertext = base64.b64encode(ciphertext)
+    return ciphertext.decode('utf-8')
+
 
 def decrypt(ciphertext, key):
-#    plaintext = ciphertext.decode('utf-8')
-    cipher = DES3.new(key, DES3.MODE_ECB)
-    plaintext = cipher.decrypt(ciphertext)
+    decoded = base64.b64decode(ciphertext)
+    cipher = DES3.new(key.encode(), DES3.MODE_ECB)
+    plaintext = cipher.decrypt(decoded)
     plaintext = unpad(plaintext)
-    plaintext.decode('utf-8')
-    return plaintext
+    return plaintext.decode('utf-8')
 
 
-key = get_random_bytes(16)
+key = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(24))
 print(f'Ключ: {key}')
 while True:
-    print('Выберите:\n\t1 - Зашифровать\n\t2 - Расшифровать')
+    print('Выберите:\n\t1 - Зашифровать\n\t2 - Расшифровать\n\t'
+          '3 - Выход из программы\n\t')
     operation = input('Ваш выбор: ')
     if operation == '1':
-        plaintext = input("Введите строку: ")
-        print(encrypt(plaintext, key))
+        plaintext = input("Введите строку для шифрования (не используя кириллицу): ")
+        print('Зашифрованная строка: ' + encrypt(plaintext, key))
+        ciphertext = encrypt(plaintext, key)
+        ans = input('Желаете сразу расшифровать эту строку? Y/N: ')
+        if ans in 'yY':
+            print('Расшифрованная строка: ' + decrypt(ciphertext, key))
+        else:
+            break
     if operation == '2':
-        ciphertext = input("Введите строку: ")
-        print(decrypt(ciphertext, key))
+        ciphertext = input("Введите шифр для расшифровки: ")
+        print('Расшифрованная строка: ' + decrypt(ciphertext, key))
+    else:
+        if operation == '3':
+            print('Выход...')
+            break
